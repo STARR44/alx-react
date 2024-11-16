f0830baca08d0137051ce7156f2aab63fe125828
STARR44 <okekeesther100@gmail.com>
2024-11-16 18:24:35 +0100

    Added dashboard_main.js to dynamically create dashboard elements and handle button clicks

---
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -0,0 +1,27 @@
+// Import jQuery and Lodash
+import $ from 'jquery';
+import _ from 'lodash';
+
+// Counter variable to track button clicks
+let count = 0;
+
+// Function to update the click count
+function updateCounter() {
+  count++;
+  $('#count').text(`${count} clicks on the button`);
+}
+
+// Create and dynamically add the required elements to the DOM
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+
+// Create a button element and bind the `debounce` function to its click event
+const $button = $('<button>Click here to get started</button>');
+$button.on('click', _.debounce(updateCounter, 500));
+$('body').append($button);
+
+// Add the count display paragraph
+$('body').append('<p id="count"></p>');
+
+// Add the copyright paragraph
+$('body').append('<p>Copyright - Holberton School</p>');
