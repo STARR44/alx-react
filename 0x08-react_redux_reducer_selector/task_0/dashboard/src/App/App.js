1bf1e9328d31d5e09d099acfffe0f618f98eda43
STARR44 <okekeesther100@gmail.com>
2024-12-05 11:32:01 +0100

    Include App component files

---
+++ 0x08-react_redux_reducer_selector/task_0/dashboard/src/App/App.js
@@ -0,0 +1,136 @@
+import React, { Component } from "react";
+import { StyleSheet, css } from "aphrodite";
+import Notifications from "../Notifications/Notifications";
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
+import Login from "../Login/Login";
+import CourseList from "../CourseList/CourseList";
+import BodySection from "../BodySection/BodySection";
+import BodySectionWithMarginBottom from "../BodySection/BodySectionWithMarginBottom";
+import { getLatestNotification } from "../utils/utils";
+import { AppContext, defaultUser } from "./AppContext";
+
+const listCourses = [
+  { id: 1, name: "ES6", credit: 60 },
+  { id: 2, name: "Webpack", credit: 20 },
+  { id: 3, name: "React", credit: 40 },
+];
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      displayDrawer: false,
+      user: defaultUser,
+      logOut: this.logOut,
+      listNotifications: [
+        // Add listNotifications to the state
+        { id: 1, type: "default", value: "New course available" },
+        { id: 2, type: "urgent", value: "New resume available" },
+        { id: 3, type: "urgent", html: { __html: getLatestNotification() } },
+      ],
+    };
+    this.handleDisplayDrawer = this.handleDisplayDrawer.bind(this);
+    this.handleHideDrawer = this.handleHideDrawer.bind(this);
+  }
+
+  logIn = (email, password) => {
+    this.setState({
+      user: {
+        email,
+        password,
+        isLoggedIn: true,
+      },
+    });
+  };
+
+  logOut = () => {
+    this.setState({
+      user: defaultUser,
+    });
+  };
+
+  // New method to remove a notification by id
+  markNotificationAsRead = (id) => {
+    this.setState((prevState) => ({
+      listNotifications: prevState.listNotifications.filter(
+        (notification) => notification.id !== id
+      ),
+    }));
+  };
+
+  handleDisplayDrawer() {
+    this.setState({ displayDrawer: true });
+  }
+
+  handleHideDrawer() {
+    this.setState({ displayDrawer: false });
+  }
+
+  handleKeyDown = (event) => {
+    if (event.ctrlKey && event.key === "h") {
+      alert("Logging you out");
+      this.state.logOut();
+    }
+  };
+
+  componentDidMount() {
+    document.addEventListener("keydown", this.handleKeyDown);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener("keydown", this.handleKeyDown);
+  }
+
+  render() {
+    const { displayDrawer, user, logOut, listNotifications } = this.state;
+
+    return (
+      <AppContext.Provider value={{ user, logOut }}>
+        <div className={css(styles.app)} data-testid="App">
+          <div className={css(styles.appContainer)}>
+            <Notifications
+              listNotifications={listNotifications} // Pass state listNotifications
+              displayDrawer={displayDrawer}
+              handleDisplayDrawer={this.handleDisplayDrawer}
+              handleHideDrawer={this.handleHideDrawer}
+              markNotificationAsRead={this.markNotificationAsRead} // Pass the function
+            />
+            <Header />
+          </div>
+          {user.isLoggedIn ? (
+            <BodySectionWithMarginBottom title="Course list">
+              <CourseList listCourses={listCourses} />
+            </BodySectionWithMarginBottom>
+          ) : (
+            <BodySectionWithMarginBottom title="Log in to continue">
+              <Login logIn={this.logIn} />
+            </BodySectionWithMarginBottom>
+          )}
+          <BodySection title="News from the School">
+            <p>Here is some news from the school. Stay tuned for updates!</p>
+          </BodySection>
+          <Footer />
+        </div>
+      </AppContext.Provider>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  app: {
+    display: "flex",
+    flexDirection: "column",
+    height: "100vh",
+  },
+  appContainer: {
+    display: "flex",
+    flexDirection: "row-reverse",
+    justifyContent: "space-between",
+    borderBottom: "3px solid rgb(194, 79, 79)",
+    fontFamily: "Arial, Helvetica, sans-serif",
+    backgroundColor: "#ffffff",
+  },
+});
+
+export default App;
